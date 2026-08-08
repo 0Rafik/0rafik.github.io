96a390bc875c6f091e0f483a9c5bec88a1719346
Kir <165975188+0Rafik@users.noreply.github.com>
2026-08-08 14:35:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const text = "かみまみた!!";
+const speed = 100;
+const delay = 1000;
+const textElement = document.getElementById('text');
+
+function type(index = 0) {
+  if (index < text.length) {
+    textElement.textContent += text.charAt(index);
+    setTimeout(() => type(index + 1), speed);
+  } else {
+    setTimeout(backspace, delay);
+  }
+}
+
+function backspace(index = text.length - 1) {
+  if (index >= 0) {
+    textElement.textContent = textElement.textContent.substring(0, textElement.textContent.length - 1);
+    setTimeout(() => backspace(index - 1), speed);
+  } else {
+    setTimeout(type, delay);
+  }
+}
+
+type();
+
+document.getElementById('image').addEventListener('click', function() {
+  const audio = new Audio('sounds/doskoi.flac');
+  audio.play();
+});
+
+window.onload = function() {
+  const welcomeMusic = document.getElementById('welcomeMusic');
+  const mainMusic = document.getElementById('mainMusic');
+  
+  welcomeMusic.play();
+  
+  welcomeMusic.onended = function() {
+    mainMusic.loop = true; 
+    mainMusic.play();
+  };
+};
