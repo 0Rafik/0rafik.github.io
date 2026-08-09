3b1921e228e0ea3fa11a9be8fa991a30706458d0
Kir <165975188+0Rafik@users.noreply.github.com>
2026-08-09 17:29:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ type();
 
 document.getElementById('image').addEventListener('click', function() {
 const now = Date.now();
-if (now - lastPlayedTime < 100) return;
+if (now - lastPlayedTime < 0) return;
 lastPlayedTime = now;
   const audio = document.getElementById('heckchu');
   if (audio) {
